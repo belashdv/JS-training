e2808fed008c81bee53779b04950d3b4bf6fe4f2
Dmitrii Belash <iscincidae@gmail.com>
2022-09-21 01:13:02 +0300

    Make some homework

---
+++ referenceTypes.js
@@ -0,0 +1,15 @@
+let a = 5;
+let b = a;
+
+a = 7;
+console.log(a);
+console.log(b);
+
+
+const arr = [1,2,3,4,5];
+const arr2 = arr;
+
+arr[0] = 10;
+console.log(arr);
+console.log(arr2);
+//same for functions and object
---
+++ variables.js
@@ -0,0 +1,8 @@
+const a = 5;
+console.log(a);
+
+let b = 6;
+console.log(b);
+
+var c = 7;
+console.log(c);
