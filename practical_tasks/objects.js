311304445b9c3995db7b39fd2e502f8071868392
Dmitrii Belash <iscincidae@gmail.com>
2022-09-22 02:21:55 +0300

    homework practice

---
+++ practical_tasks/objects.js
@@ -0,0 +1,71 @@
+
+/*
+let user = {
+    name: "John",
+    surname: "Smith",
+}
+console.log(user);
+
+user.name = "Pete";
+console.log(user);
+
+delete user.name;
+console.log(user);
+
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  }
+
+let sum = 0;
+for (let key in salaries) {
+    sum += salaries[key];
+}
+  console.log(sum);
+
+
+
+  let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  }
+  
+  multiplyNumeric(menu);
+
+  function multiplyNumeric(obj) {
+    for (let key in obj) {
+       if (typeof obj[key] == "number") {  
+       obj[key] *= 2;
+    }
+  }
+}
+  console.log(menu);
+*/
+
+
+
+
+
+
+
+
+  /* Homework part*/
+
+  
+var car = {
+    color: 'Black',
+  };
+console.log(car);
+
+car.color = 'Green';
+car.power = showPower(138);
+
+
+function showPower(a) {
+    console.log(`${a}kW`);
+  };
+
+console.log(car);
