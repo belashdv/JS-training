82f7a07905ab881475e5842d97f0ca89dede0a66
Dmitrii Belash <iscincidae@gmail.com>
2022-10-17 17:56:50 +0300

    working on my test

--- wdio/test/specs/test.spec.js
+++ wdio/test/specs/test.spec.js
@@ -1,5 +1,5 @@
 describe("Test suite", () => {
-
+   
     it("First test", async () => {
         await browser.url("https://pastebin.com");
         const pageTitle = await browser.getTitle();
@@ -8,8 +8,19 @@ describe("Test suite", () => {
     });
 
     it ("Second test", async () => {
-        await $("div.form-group").click();
-        await $("div.form-group").setValue("Hello from WebDriver"); 
+        await $("div>textarea").click();
+        await $("div>textarea").setValue("Hello from WebDriver"); 
+        //await $("#select2-postform-expiration-container").click();
+        //await $(`div[class="col-sm-9 field-wrapper"]>#postform-expiration`).selectByVisibleText('10 Minutes');
+        //await $("#select2-postform-expiration-container").getValue();
+        await $("#postform-name").click();
+        await $("#postform-name").setValue("helloweb"); 
+        const postformNameValue = await $(`//input[@id='postform-name']`).getValue();
+        expect(postformNameValue).toEqual('helloweb');
+        await $("//button[text()='Create New Paste']").click();
+        
+        const url = await browser.getUrl();
+        console.log(url);
     });
 
 })
